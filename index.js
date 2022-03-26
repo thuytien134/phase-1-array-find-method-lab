c6aae086796ba85d5b6b0a1ce38daf98eb8b81f9
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-03-25 21:01:37 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    if (record.find(record => record.result === "W")){
+        return '2015'
+        
+    } else {undefined
+        
+    } 
+}
